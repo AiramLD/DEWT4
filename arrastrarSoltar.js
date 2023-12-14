c392fd825c65d27a0a23833d5e791289b8916be3
AiramLD <airamluisdiaz629@gmail.com>
2023-12-14 12:05:11 +0000

    drag and drop with task list

--- arrastrarSoltar.js
+++ arrastrarSoltar.js
@@ -1,24 +1,23 @@
 let parent = null;
+
 function dragStart(event) {
   event.dataTransfer.setData('text/plain', event.target.id);
   parent = event.target.parentElement;
   event.target.classList.add('selected');
-
 }
+
 function dragEnd(event) {
-  element.classList.remove('selected');
-}
-
+  event.target.classList.remove('selected');
+ }
+ 
 function dragOver(event) {
-  if (event.currentTarget !== parent) {
+    if (event.currentTarget !== parent) {
     event.preventDefault();
   }
 }
-
 
 function drop(event) {
   const id = event.dataTransfer.getData('text/plain');
   const element = document.getElementById(id);
   event.currentTarget.append(element);
 }
-
